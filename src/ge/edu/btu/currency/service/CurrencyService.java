0cc35c19eae224c378cf0fed76065c62cfd821bc
RuDD8 <aleksandre.kazanjiani.1@btu.edu.ge>
2020-11-10 20:41:59 +0400

    commit'

---
+++ src/ge/edu/btu/currency/service/CurrencyService.java
@@ -0,0 +1,8 @@
+package ge.edu.btu.currency.service.impl;
+
+public interface CurrencyService {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/ge/edu/btu/currency/service/impl/CurrencyServiceImpl.java
@@ -0,0 +1,7 @@
+package ge.edu.btu.currency.service.impl;
+
+public class CurrencyServiceImpl implements ge.edu.btu.currency.service.impl.CurrencyService {
+    public static void main(String[] args) {
+
+    }
+}
